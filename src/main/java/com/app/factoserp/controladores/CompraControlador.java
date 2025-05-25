bc699e13438bd9e6e68f9ce0e3ea2e4419437ba4
Samuel Bercian <samuelbercian1706@gmail.com>
2025-05-25 12:13:32 -0600

    Compra detalle y compra finalizado

--- src/main/java/com/app/factoserp/controladores/CompraControlador.java
+++ src/main/java/com/app/factoserp/controladores/CompraControlador.java
@@ -1,18 +1,25 @@
 package com.app.factoserp.controladores;
 
+import java.time.LocalDate;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import com.app.factoserp.modelos.Compra;
+import com.app.factoserp.modelos.Usuarios;
 import com.app.factoserp.servicios.CompraServicio;
-import org.springframework.web.bind.annotation.GetMapping;
+import com.app.factoserp.servicios.UsuarioServicio;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class CompraControlador {
     
     @Autowired
     private CompraServicio compraServicio;
+
+    @Autowired UsuarioServicio usuarioServicio;
 
     @GetMapping("/compras")
         public String mostrarCompras(Model model){
@@ -20,4 +27,40 @@ public class CompraControlador {
             model.addAttribute("compras", compras);
             return "compra";
         }
+    
+    @PostMapping("/compras/nueva")
+    public String crearCompra() {
+
+        Compra compra = new Compra();
+        // Establece la hora actual
+        compra.setFechaCreacion(LocalDate.now());
+
+        // Obtiene usuario autenticado
+        org.springframework.security.core.Authentication authentication = 
+        org.springframework.security.core.context.SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName(); // Obtiene el username
+
+        Usuarios usuario = usuarioServicio.buscarEmail(username);
+        
+        compra.setComprador(usuario);
+        compraServicio.guardarCompra(compra);
+
+        return "redirect:/compras/editar/" + compra.getId();
+    }
+
+    @GetMapping("/compras/eliminar/{id}")
+        public String eliminarCompra(@PathVariable int id) {
+        compraServicio.borrarCompra(id);
+        return "redirect:/compras";
+    }
+    
+    @GetMapping("/compras/recibir/{id}")
+    public String recibirCompra(@PathVariable int id) {
+        Compra compra = compraServicio.buscarCompra(id);
+        if (compra != null) {
+            compra.setFechaEntrega(LocalDate.now()); // Asegúrate que este campo existe
+            compraServicio.guardarCompra(compra);
+        }
+        return "redirect:/compras";
+    }
 }
--- src/main/java/com/app/factoserp/controladores/CompraDetalleControlador.java
+++ src/main/java/com/app/factoserp/controladores/CompraDetalleControlador.java
@@ -1,18 +1,19 @@
 package com.app.factoserp.controladores;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import com.app.factoserp.modelos.Compra;
 import com.app.factoserp.modelos.CompraDetalle;
+import com.app.factoserp.modelos.Producto;
 import com.app.factoserp.servicios.CompraDetalleServicio;
 import com.app.factoserp.servicios.CompraServicio;
+import com.app.factoserp.servicios.ProductoServicio;
 
 @Controller
 @RequestMapping("/compras")
@@ -24,6 +25,9 @@ public class CompraDetalleControlador {
     @Autowired
     private CompraDetalleServicio compraDetalleServicio;
 
+    @Autowired
+    private ProductoServicio productoServicio;
+
     @GetMapping("/editar/{id}")
     public String mostrarFormularioEditarCompra(@PathVariable int id, Model model) {
         Compra compra = compraServicio.buscarCompra(id);
@@ -32,9 +36,61 @@ public class CompraDetalleControlador {
         }
 
         List<CompraDetalle> detalles = compraDetalleServicio.buscarPorCompra(compra);
+        List<Producto> productos = productoServicio.listarProductos();
 
         model.addAttribute("compra", compra);
         model.addAttribute("compraDetalle", detalles);
+        model.addAttribute("productos", productos);
         return "editarCompra";
+    }
+
+    @PostMapping("/detalle/agregar")
+    public String guardarCompraDetalle(
+            @RequestParam("compraId") int compraId,
+            @RequestParam("productoId") int productoId,
+            @RequestParam("cantidad") int cantidad) {
+
+        CompraDetalle compraDetalle = new CompraDetalle();
+        Producto producto = productoServicio.buscarProducto(productoId);
+        Compra compra = compraServicio.buscarCompra(compraId);
+
+        compraDetalle.setCompra(compra);
+        compraDetalle.setProducto(producto);
+        compraDetalle.setCantidad(cantidad);
+
+        double precioTotal = producto.getPrecioUnitario() * cantidad;
+        compra.setTotal(compra.getTotal() + precioTotal);
+
+        compraServicio.actualizarCompra(compra);
+        compraDetalleServicio.guardarCompraDetalle(compraDetalle);
+
+        productoServicio.aumentarStock(producto, cantidad); // al comprar, se aumenta el stock
+
+        return "redirect:/compras/editar/" + compraId;
+    }
+
+    @GetMapping("/detalle/eliminar/{id}")
+    public String eliminarCompraDetalle(@PathVariable("id") int id) {
+        CompraDetalle compraDetalle = compraDetalleServicio.buscarCompraDetalle(id);
+        Compra compra = compraDetalle.getCompra();
+
+        if (compraDetalle != null) {
+            compraDetalleServicio.eliminarCompraDetalle(id);
+            Producto producto = compraDetalle.getProducto();
+
+            compra.setFechaModificacion(LocalDate.now());
+            compra.setTotal(compra.getTotal() - (producto.getPrecioUnitario() * compraDetalle.getCantidad()));
+
+            productoServicio.disminuirStock(producto.getId(), compraDetalle.getCantidad()); // al eliminar compra, se quita del stock
+            compraServicio.actualizarCompra(compra);
+        }
+
+        return "redirect:/compras/editar/" + compra.getId();
+    }
+
+    @GetMapping("/eliminar/{id}")
+    public String eliminarCompra(@PathVariable int id) {
+        compraServicio.borrarCompra(id);
+        return "redirect:/compras";
     }
 }
